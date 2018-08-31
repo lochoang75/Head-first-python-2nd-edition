641fe6781546e51a89f4b179d027c8af058e133c
Hoang Van Loc <lochoang75@gmail.com>
2018-08-31 17:19:36 +0700

    [tutorial] some example from book

---
+++ vowels2.py
@@ -0,0 +1,9 @@
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("Provide a word to search for vowels: ")
+found = []
+for letter in word:
+    if letter not in found:
+        if letter in vowels:
+            found.append(letter)
+for vowel in found:
+    print(vowel)
